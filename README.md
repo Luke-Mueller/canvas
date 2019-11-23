a63637232a321ee11c694cfea6ef50e71f84a659
Luke Mueller <muelll79@hotmail.com>
2019-11-23 12:37:48 -0700

    updates readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-This is practice using the canvas html element with JavaScript to render new backgrounds.
+This is practice using the canvas API with JavaScript to render new backgrounds.
 
-Comment in and out any pattern I've created in the index.js file to view them.
+Comment in any pattern I've created in the index.js file and open the index.html file to view them.
--- index.js
+++ index.js
@@ -28,17 +28,20 @@ function draw() {
 
   // // bubbles ---------------------------------
 
-  for(let i = 0; i < 1000; i++) {
+  for(let i = 0; i < 2000; i++) {
     ctx.beginPath();
     const x = Math.random() * wx;
     const y = Math.random() * wy;
-    const r = Math.random() * 15 + 5;
+    const r = Math.random() * 10 + 3;
     const sAngle = 0;
     const eAngle = Math.PI * 2;
 
+    ctx.shadowOffsetY = -1;
+    ctx.shadowColor = 'rgba(150, 150, 150, 0.5)';
+
     const radgrad = ctx.createRadialGradient(x, y, r, x, y-8, r);
     radgrad.addColorStop(0, 'rgb(200, 200, 200)')
-    radgrad.addColorStop(0.8, 'rgba(203, 205, 180, 0.5)')
+    radgrad.addColorStop(0.9, 'rgba(203, 205, 180, 0.5)')
     radgrad.addColorStop(1, 'rgba(198, 200, 175, 0.5)')
 
     ctx.fillStyle = radgrad;
@@ -48,11 +51,11 @@ function draw() {
 
 
   //  graph ---------------------------------
-  // for(let i = 0; i < 7; i++) {
-  //   for(let j = 0; j < 7; j++) {
+  // for(let i = 0; i < 6; i++) {
+  //   for(let j = 0; j < 6; j++) {
   //     ctx.fillStyle = 'rgb(' + (
-  //       Math.floor(255 - 31.8 * i) + ', 0, ' +
-  //       Math.floor(255 - 31.8 * j) + ')'
+  //       Math.floor(255 - 42.5 * i) + ', 0, ' +
+  //       Math.floor(255 - 42.5 * j) + ')'
   //     )
   //     ctx.fillRect(j * 50, i * 50, 50, 50)
   //   }
