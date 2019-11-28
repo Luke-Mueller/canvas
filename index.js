48c8401b00ec3f835a395bb6bd0d1102b1f17b77
Luke Mueller <muelll79@hotmail.com>
2019-11-28 00:52:56 -0700

    starts work on bubbles animation function

--- index.js
+++ index.js
@@ -1,89 +1,129 @@
-const body = document.querySelector('body');
-body.addEventListener(onload, draw());
-
-function draw() {
-  const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
-  const wx = window.innerWidth;
-  const wy = window.innerHeight;
-  canvas.width = wx;
-  canvas.height = wy; 
+//  Canvas dimensions
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+let wx = window.innerWidth;
+let wy = window.innerHeight;
+canvas.width = wx;
+canvas.height = wy; 
 
 
-  //  stars pattern ---------------------------------
+//  Stars pattern ---------------------------------
 
-  // for(let i = 0; i < 5000; i++) {
-  //   ctx.beginPath();
-  //   const x = Math.random() * wx;
-  //   const y = Math.random() * wy;
-  //   const r = Math.random() * 1.5;
-  //   const sAngle = 0;
-  //   const eAngle = Math.PI * 2;
+// for(let i = 0; i < 5000; i++) {
+//   ctx.beginPath();
+//   const x = Math.random() * wx;
+//   const y = Math.random() * wy;
+//   const r = Math.random() * 1.5;
+//   const sAngle = 0;
+//   const eAngle = Math.PI * 2;
+//   ctx.fillStyle = 'rgba(198, 200, 175, ' + Math.random() + ')';
+//   ctx.arc(x, y, r, sAngle, eAngle, false);
+//   ctx.fill();
+// }
 
-  //   ctx.fillStyle = 'rgba(198, 200, 175, ' + Math.random() + ')';
-  //   ctx.arc(x, y, r, sAngle, eAngle, false);
-  //   ctx.fill();
-  // }
 
+//  Bubbles pattern ---------------------------------
 
-  //  bubbles pattern ---------------------------------
+const grav = -1;
+class Bubble {
+  constructor() {
+    this.x = Math.random() * wx;
+    this.y = 0;
+    this.r = Math.random() * 10 + 3;
+    this.dy = Math.random() * 2;
+    this.dx = Math.round((Math.random() - 0.5) * 10);
+    this.vel = Math.random() / 5;
+    this.sAngle = 0;
+    this.eAngle = Math.PI * 2;
+    this.update = () => {
+      //  Outer bubble shadow
+      ctx.shadowOffsetY = -1;
+      ctx.shadowColor = 'rgb(180, 180, 180)';
 
-  // for(let i = 0; i < 50; i++) {
-  //   const x = Math.random() * wx;
-  //   const y = Math.random() * wy;
-  //   const r = Math.random() * 10 + 3;
-  //   const sAngle = 0;
-  //   const eAngle = Math.PI * 2;
+      //  Outer bubble radial gradient fillStyle
+      const radgrad = ctx.createRadialGradient(this.x, this.y, this.r, this.x, this.y - 8, this.r / 2);
+      radgrad.addColorStop(0, 'rgba(200, 200, 200, 0.5)');
+      radgrad.addColorStop(0.2, 'rgba(180, 180, 180, 0.3)');
+      radgrad.addColorStop(0.4, 'rgba(160, 160, 160, 0.2)');
+      radgrad.addColorStop(0.6, 'rgba(150, 150, 150, 0.1)');
+      radgrad.addColorStop(0.8, 'rgba(150, 150, 150, 0.05)');
+      radgrad.addColorStop(1, 'rgba(150, 150, 150, 0.02)');
+      //  Outer bubble
+      ctx.beginPath();
+      ctx.fillStyle = radgrad;
+      ctx.strokeStyle = 'rgba(200, 200, 200, 0.2)';
+      ctx.lineWidth = 0.2;
+      ctx.arc(this.x, this.y, this.r, this.sAngle, this.eAngle, false);
+      ctx.fill();
+      ctx.stroke();
+      //  Inner bubble
+      ctx.beginPath();
+      ctx.fillStyle = 'rgba(150, 150, 150, 0.3)';
+      ctx.arc(this.x, this.y + this.r - 2, this.r / 10, this.sAngle, this.eAngle, false);
+      ctx.fill();
+    }
+  };
+};
 
-  //   ctx.shadowOffsetY = -1;
-  //   ctx.shadowColor = 'rgb(180, 180, 180)';
+//  Render bubbles 
 
-  //   const radgrad = ctx.createRadialGradient(x, y, r, x, y - 8, r / 2);
-  //   radgrad.addColorStop(0, 'rgba(200, 200, 200, 0.5)')
-  //   radgrad.addColorStop(0.2, 'rgba(180, 180, 180, 0.3)')
-  //   radgrad.addColorStop(0.4, 'rgba(160, 160, 160, 0.2)')
-  //   radgrad.addColorStop(0.6, 'rgba(150, 150, 150, 0.1)')
-  //   radgrad.addColorStop(0.8, 'rgba(150, 150, 150, 0.05)')
-  //   radgrad.addColorStop(1, 'rgba(150, 150, 150, 0.02)')
+const bubbles = [];
 
-  //   ctx.beginPath();
-  //   ctx.fillStyle = radgrad;
-  //   ctx.strokeStyle = 'rgba(200, 200, 200, 0.2)'
-  //   ctx.lineWidth = 0.2;
-  //   ctx.arc(x, y, r, sAngle, eAngle, false);
-  //   ctx.fill();
-  //   ctx.stroke();
-
-  //   ctx.beginPath();
-  //   ctx.fillStyle = 'rgba(150, 150, 150, 0.3)';
-  //   ctx.arc(x, y + r - 2, r/10, sAngle, eAngle, false)
-  //   ctx.fill();
-  // }
-
-    /*
-
-    to animate bubbles...   if(y === 0) y = wy
-
-    */
-
-    
-  //  palette pattern ---------------------------------
-
-  const xlength = wx / 50;
-  const ylength = wy / 50;
-  const rgbX = 255 / xlength;
-  const rgbY = 255 / ylength;
-
-  for(let i = 0; i < ylength; i++) {
-    for(let j = 0; j < xlength; j++) {
-      ctx.fillStyle = 'rgb(' + (
-        Math.floor(255 - rgbY * i) + ', 0, ' +
-        Math.floor(255 - rgbX * j) + ')'
-      )
-      ctx.strokeStyle = '#777777';
-      // ctx.beginPath();
-      ctx.fillRect(j * 50, i * 50, 50, 50);
-      ctx.strokeRect(j * 50, i * 50, 50, 50);
+function render() {
+  if(wx !== window.innerWidth || wy !== window.innerHeight) {
+    wx = window.innerWidth;
+    wy = window.innerHeight;
+    canvas.width = wx;
+    canvas.height = wy;
+  }
+  ctx.clearRect(0, 0, wx, wy);
+  for (let i = 0; i < bubbles.length; i++) {
+    bubbles[i].update();
+    bubbles[i].y += bubbles[i].dy;
+    bubbles[i].x += bubbles[i].dx;
+    if (bubbles[i].y + bubbles[i].r >= wy) {
+      bubbles[i].dy = -bubbles[i].dy * grav;
+    } else {
+      bubbles[i].dy += bubbles[i].vel;
+    }    
+    if(bubbles[i].x + bubbles[i].r > wx || bubbles[i].x - bubbles[i].r < 0){
+        bubbles[i].dx = -bubbles[i].dx;
     }
   }
+  requestAnimationFrame(render);
 }
+
+render();
+
+setInterval(()=> {
+  if(bubbles.length < 50) {
+    bubbles.push(new Bubble());
+  };
+}, 500);
+
+  /*
+
+  to animate bubbles...   if(y === 0) y = wy + r
+
+  */
+
+  
+//  palette pattern ---------------------------------
+
+// const xlength = wx / 50;
+// const ylength = wy / 50;
+// const rgbX = 255 / xlength;
+// const rgbY = 255 / ylength;
+
+// for(let i = 0; i < ylength; i++) {
+//   for(let j = 0; j < xlength; j++) {
+//     ctx.fillStyle = 'rgb(' + (
+//       Math.floor(255 - rgbY * i) + ', 0, ' +
+//       Math.floor(255 - rgbX * j) + ')'
+//     )
+//     ctx.strokeStyle = '#777777';
+//     // ctx.beginPath();
+//     ctx.fillRect(j * 50, i * 50, 50, 50);
+//     ctx.strokeRect(j * 50, i * 50, 50, 50);
+//   }
+// }
