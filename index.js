812fac28f4ef47f35b45cdcb0d89cf3a4c3098be
Luke Mueller <muelll79@hotmail.com>
2019-11-27 22:03:48 -0700

    scales palette pattern

--- index.js
+++ index.js
@@ -28,37 +28,37 @@ function draw() {
 
   //  bubbles pattern ---------------------------------
 
-  for(let i = 0; i < 2000; i++) {
-    const x = Math.random() * wx;
-    const y = Math.random() * wy;
-    const r = Math.random() * 10 + 3;
-    const sAngle = 0;
-    const eAngle = Math.PI * 2;
+  // for(let i = 0; i < 50; i++) {
+  //   const x = Math.random() * wx;
+  //   const y = Math.random() * wy;
+  //   const r = Math.random() * 10 + 3;
+  //   const sAngle = 0;
+  //   const eAngle = Math.PI * 2;
 
-    ctx.shadowOffsetY = -1;
-    ctx.shadowColor = 'rgb(180, 180, 180)';
+  //   ctx.shadowOffsetY = -1;
+  //   ctx.shadowColor = 'rgb(180, 180, 180)';
 
-    const radgrad = ctx.createRadialGradient(x, y, r, x, y - 8, r / 2);
-    radgrad.addColorStop(0, 'rgba(200, 200, 200, 0.5)')
-    radgrad.addColorStop(0.1, 'rgba(180, 180, 180, 0.3)')
-    radgrad.addColorStop(0.4, 'rgba(160, 160, 160, 0.2)')
-    radgrad.addColorStop(0.6, 'rgba(150, 150, 150, 0.1)')
-    radgrad.addColorStop(0.8, 'rgba(150, 150, 150, 0.05)')
-    radgrad.addColorStop(1, 'rgba(150, 150, 150, 0.02)')
+  //   const radgrad = ctx.createRadialGradient(x, y, r, x, y - 8, r / 2);
+  //   radgrad.addColorStop(0, 'rgba(200, 200, 200, 0.5)')
+  //   radgrad.addColorStop(0.2, 'rgba(180, 180, 180, 0.3)')
+  //   radgrad.addColorStop(0.4, 'rgba(160, 160, 160, 0.2)')
+  //   radgrad.addColorStop(0.6, 'rgba(150, 150, 150, 0.1)')
+  //   radgrad.addColorStop(0.8, 'rgba(150, 150, 150, 0.05)')
+  //   radgrad.addColorStop(1, 'rgba(150, 150, 150, 0.02)')
 
-    ctx.beginPath();
-    ctx.fillStyle = radgrad;
-    ctx.strokeStyle = 'rgba(200, 200, 200, 0.3)'
-    ctx.lineWidth = 0.2;
-    ctx.arc(x, y, r, sAngle, eAngle, false);
-    ctx.fill();
-    ctx.stroke();
+  //   ctx.beginPath();
+  //   ctx.fillStyle = radgrad;
+  //   ctx.strokeStyle = 'rgba(200, 200, 200, 0.2)'
+  //   ctx.lineWidth = 0.2;
+  //   ctx.arc(x, y, r, sAngle, eAngle, false);
+  //   ctx.fill();
+  //   ctx.stroke();
 
-    ctx.beginPath();
-    ctx.fillStyle = 'rgba(150, 150, 150, 0.3)';
-    ctx.arc(x, y + r - 2, r/10, sAngle, eAngle, false)
-    ctx.fill();
-  }
+  //   ctx.beginPath();
+  //   ctx.fillStyle = 'rgba(150, 150, 150, 0.3)';
+  //   ctx.arc(x, y + r - 2, r/10, sAngle, eAngle, false)
+  //   ctx.fill();
+  // }
 
     /*
 
@@ -66,19 +66,24 @@ function draw() {
 
     */
 
-
-
-
-
+    
   //  palette pattern ---------------------------------
 
-  // for(let i = 0; i < 6; i++) {
-  //   for(let j = 0; j < 6; j++) {
-  //     ctx.fillStyle = 'rgb(' + (
-  //       Math.floor(255 - 42.5 * i) + ', 0, ' +
-  //       Math.floor(255 - 42.5 * j) + ')'
-  //     )
-  //     ctx.fillRect(j * 50, i * 50, 50, 50)
-  //   }
-  // }
+  const xlength = wx / 50;
+  const ylength = wy / 50;
+  const rgbX = 255 / xlength;
+  const rgbY = 255 / ylength;
+
+  for(let i = 0; i < ylength; i++) {
+    for(let j = 0; j < xlength; j++) {
+      ctx.fillStyle = 'rgb(' + (
+        Math.floor(255 - rgbY * i) + ', 0, ' +
+        Math.floor(255 - rgbX * j) + ')'
+      )
+      ctx.strokeStyle = '#777777';
+      // ctx.beginPath();
+      ctx.fillRect(j * 50, i * 50, 50, 50);
+      ctx.strokeRect(j * 50, i * 50, 50, 50);
+    }
+  }
 }
